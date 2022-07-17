59754e7ed070753c11a051f73a253dc9aa21e520
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2022-07-17 21:10:11 +0300

    add prompt in console

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
 use std::process::Command;
 
 fn main() {
     let mut input = String::new();
+
+    stdout().write("Console started, press command:\n".as_bytes());
     stdin().read_line(&mut input).unwrap();
 
     let command = input.trim();
